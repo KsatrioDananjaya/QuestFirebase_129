d87216cd1854b5eb81fbe3c29c31958b40fbbd45
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-01-13 13:02:01 +0700

    melengkapi kontainer

--- app/src/main/java/com/example/questfirebase/di/MhsContainer.kt
+++ app/src/main/java/com/example/questfirebase/di/MhsContainer.kt
@@ -1,4 +1,15 @@
 package com.example.questfirebase.di
 
+import com.example.questfirebase.repo.NetworkRepoMhs
+import com.example.questfirebase.repo.RepoMhs
+import com.google.firebase.firestore.FirebaseFirestore
+
+interface  AppContainer{
+    val repoMhs: RepoMhs
+}
 class MhsContainer {
+    private  val firebase: FirebaseFirestore = FirebaseFirestore.getInstance()
+    override val repoMhs : RepoMhs by lazy {
+        NetworkRepoMhs(firebase)
+    }
 }
