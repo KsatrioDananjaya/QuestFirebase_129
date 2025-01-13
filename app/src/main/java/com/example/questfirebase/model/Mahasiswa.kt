5fcc11b3c857165a7c744ef3ed5f8caf1aeeed8f
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-01-13 12:53:49 +0700

    membuat data class

--- app/src/main/java/com/example/questfirebase/model/Mahasiswa.kt
+++ app/src/main/java/com/example/questfirebase/model/Mahasiswa.kt
@@ -1,4 +1,11 @@
 package com.example.questfirebase.model
 
-class Mahasiswa {
+data class Mahasiswa (
+    val nim: String,
+    val nama: String,
+    val gender: String,
+    val kelas: String,
+    val angkatan: String,
+    val alamat: String,
+){
 }
