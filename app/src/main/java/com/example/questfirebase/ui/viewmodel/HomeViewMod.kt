3d8d38c23445d5d1157b70117a77e21faf1fe93a
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-01-13 14:02:02 +0700

    lengkapin homeviewmodel

--- app/src/main/java/com/example/questfirebase/ui/viewmodel/HomeViewMod.kt
+++ app/src/main/java/com/example/questfirebase/ui/viewmodel/HomeViewMod.kt
@@ -1,7 +1,16 @@
 package com.example.questfirebase.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import com.example.questfirebase.model.Mahasiswa
-import java.lang.Exception
+import com.example.questfirebase.repo.RepoMhs
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
@@ -9,6 +18,30 @@ sealed class HomeUiState {
     object Loading : HomeUiState()
 }
 
-class HomeViewMod {
-
+class HomeViewMod(
+    private val mhs: RepoMhs
+) {
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+    private set
+    init {
+        getMhs()
+    }
+    fun getMhs() {
+        viewModelScope.launch {
+            mhs.getAllMahasiswa()
+                .onStart{
+                    mhsUIState = HomeUiState.Loading
+                }
+                .catch{
+                    mhsUIState = HomeUiState.Error(it)
+                }
+                .collect{
+                    mhsUIState = if (it.isEmpty()) {
+                        HomeUiState.Error(Exception("Belum ada daftar mahasiswa"))
+                    }else{
+                        HomeUiState.Success(it)
+                    }
+                }
+        }
+    }
 }
